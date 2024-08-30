0106493492b246945b446747b2196384aa6015a7
Arturo Juarez <ajrobles98@gmail.com>
2024-08-30 12:53:28 -0600

    feat : update components

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
+import { getPokemon } from "../api";
+import { useNavigate } from "react-router-dom";
 
 export default function PokemonCard({ name }) {
   const [pokemon, setPokemon] = useState({});
+  const navigate = useNavigate();
 
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-      .then((response) => response.json())
-      .then((jsonResponse) => {
-        setPokemon(jsonResponse);
-      })
-      .catch((error) => {
-        console.log("fetch pokemon error :", error);
-      });
+    getPokemon(name)
+      .then((pokemon) => setPokemon(pokemon))
+      .catch((error) => console.log("fetch pokemon error :", error));
   }, []);
 
   return (
-    <article className="p-6 bg-white rounded-md text-center drop-shadow-lg cursor-pointer transition duration-300 hover:scale-110 hover:text-yellow-300 hover:bg-gray-800">
+    <article
+      onClick={() => {
+        navigate(`/pokemon/${name}`);
+      }}
+      className="p-6 bg-white rounded-md text-center drop-shadow-lg cursor-pointer transition duration-300 hover:scale-110 hover:text-yellow-300 hover:bg-gray-800"
+    >
       <img
         src={pokemon.sprites?.other["official-artwork"].front_default}
         alt=""
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import PokemonPage from "./pages/PokemonPage";
+
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/pokemon/:name",
+    element: <PokemonPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   //<StrictMode>
-  <App />
+  <RouterProvider router={router} />
   //</StrictMode>,
 );
