e1c62edbb4aff4f8dffb8cc5717d6feea5ace6da
Arturo Juarez <ajrobles98@gmail.com>
2024-08-30 13:02:41 -0600

    feat : adding style to pokemon page

--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -21,19 +21,29 @@ export default function PokemonPage() {
   }
 
   return (
-    <main>
+    <main className="w-screen h-screen flex items-center justify-center flex-col bg-black p-10">
       <img
         src={pokemon.sprites?.other["official-artwork"].front_default}
         alt={name}
+        className="w-128"
       />
-      <h1>{name}</h1>
+      <h1 className=" capitalize first-letter:uppercase text-[3rem] text-yellow-300">
+        {name}
+      </h1>
       <section className="flex flex-col">
-        <span>{pokemon.height}</span>
-        <span>{pokemon.weight}</span>
+        <span className="text-white">Height : {pokemon.height}</span>
+        <span className="text-white">Weight : {pokemon.weight}</span>
       </section>
-      <section>
+      <section className="flex flex-row gap-4 pt-3">
         {pokemon.types?.map((type) => {
-          return <span key={type.slot}>{type?.type?.name}</span>;
+          return (
+            <span
+              className="text-white bg-blue-600 p-1 rounded-md"
+              key={type.slot}
+            >
+              {type?.type?.name}
+            </span>
+          );
         })}
       </section>
     </main>
